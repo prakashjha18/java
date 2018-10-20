1bbc5e75b0fbfd9c2dd4166d51c7b6fab88d428d
prakashjha18 <41507803+prakashjha18@users.noreply.github.com>
2018-10-20 10:54:26 -0700

    anagrams

---
+++ anagram.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class anagram {
+static boolean isAnagram(String a, String b) {
+
+        if (a.length() != b.length()) return false;
+        
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        int sum=0;
+        for(int i=0;i<a.length();i++){
+           sum=sum+(int)a.charAt(i)-(int)b.charAt(i); 
+        }
+    if(sum==0){
+        return true;
+    }
+    else {
+        return false;
+    }
+        
+}
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
