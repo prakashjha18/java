b6075117af489536424eafe315f666a613248934
prakashjha18 <41507803+prakashjha18@users.noreply.github.com>
2018-10-29 11:35:19 -0700

    nested class

---
+++ InnerClassDemo.java
@@ -0,0 +1,26 @@
+class Outer {
+int outer_x = 100;
+void test() {
+Inner inner = new Inner();
+inner.display();
+}
+// this is an inner class
+class Inner {
+void display() {
+System.out.println("display: outer_x = "
++ outer_x);
+}
+}
+}
+
+class InnerClassDemo {
+public static void main(String args[])
+{
+Outer outer = new Outer();
+outer.test();
+}
+}
+
+   
+    
+        
