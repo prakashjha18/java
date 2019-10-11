eaafa6a0783657fe5c4c355e986823bef5fffb7d
prakash jha <prkjha25@gmail.com>
2019-10-11 14:13:19 +0530

    rounding of students based on certain parameters

---
+++ Problem Solving/Easy/gradingstudents.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+    // Write your code here
+        List<Integer> rounded = new ArrayList<>();
+        int rem=0;
+        for(Integer grade : grades){
+            if(grade<38){
+                rounded.add(grade);
+            }
+            else{
+                rem = grade%5;
+                if(5-rem<3){
+                    rounded.add(grade+(5-rem));
+                }
+                else{
+                    rounded.add(grade);
+                }
+            }
+        }
+        return rounded;
+    }
+
+}
+
+public class gradingstudents {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = new ArrayList<>();
+
+        for (int i = 0; i < gradesCount; i++) {
+            int gradesItem = Integer.parseInt(bufferedReader.readLine().trim());
+            grades.add(gradesItem);
+        }
+
+        List<Integer> result = Result.gradingStudents(grades);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
