da622a7b2b7f521de50a1025f79b361143855173
prakash jha <prkjha25@gmail.com>
2019-10-15 21:42:10 +0530

    min no. of steps to reach to a certain page

---
+++ Problem Solving/Easy/drawingbook.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class drawingbook {
+
+    static int pageCount(int n, int p) {
+        int page1 = Math.abs((p) / 2);
+        if(n%2==0){
+            n++;
+        }
+        int page2 = Math.abs((p - (n)) / 2);
+        return page1 < page2 ? page1 : page2;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int p = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int result = pageCount(n, p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
