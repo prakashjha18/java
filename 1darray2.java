61c169d328c259daa39972139b09ddde73f770b4
jrajesh99 <42646853+jrajesh99@users.noreply.github.com>
2019-10-23 15:57:55 +0530

    Update 1darray2.java

--- 1darray2.java
+++ 1darray2.java
@@ -6,10 +6,10 @@ public class 1darray2 {
         if (game == null) {
             return false;
         }
-        return isSolvable(leap, game, 0);
+        return isSolvable(a, game, 0);
     }
 
-    private static boolean isSolvable(int leap, int[] game, int i) {
+    private static boolean isSolvable(int a, int[] game, int i) {
         // Base Cases
         if (i >= game.length) {
             return true;
@@ -20,7 +20,7 @@ public class 1darray2 {
         game[i] = 1; // marks as visited
 
         // Recursive Cases (Tries +m first to try to finish game quickly)
-        return isSolvable(leap, game, i + leap)
+        return isSolvable(leap, game, i + a)
             || isSolvable(leap, game, i + 1)
             || isSolvable(leap, game, i - 1);
     }
