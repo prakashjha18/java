0188d2054b0992c6f97e331952f15f1e6dd5f1c1
prakash jha <prkjha25@gmail.com>
2019-10-31 20:56:46 +0530

    https://www.hackerrank.com/challenges/taum-and-bday/problem

---
+++ Problem Solving/Easy/taumbday.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'taumBday' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER b
+     *  2. INTEGER w
+     *  3. INTEGER bc
+     *  4. INTEGER wc
+     *  5. INTEGER z
+     */
+
+    public static long taumBday(long b, long w, long bc, long wc, long z) {
+    // Write your code here
+        long y=0,m=0;
+        long x=(b*bc)+(w*wc);
+        y=((b+w)*bc)+(w*z);
+        m=((b+w)*wc)+(b*z);
+        return Math.min(x,Math.min(y,m));
+    }
+
+}
+
+public class taumbday {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            int b = Integer.parseInt(firstMultipleInput[0]);
+
+            int w = Integer.parseInt(firstMultipleInput[1]);
+
+            String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            int bc = Integer.parseInt(secondMultipleInput[0]);
+
+            int wc = Integer.parseInt(secondMultipleInput[1]);
+
+            int z = Integer.parseInt(secondMultipleInput[2]);
+
+            long result = Result.taumBday(b, w, bc, wc, z);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
