c8dde8e7b8a3e163b175bcbf11cccd2215fb15f1
prakash jha <prkjha25@gmail.com>
2019-12-05 14:10:36 +0530

    if a string in pangram or not

---
+++ Problem Solving/Easy/pangram.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class pangram {
+
+    // Complete the pangrams function below.
+    static String pangrams(String s) {
+        s= s.toLowerCase();
+        HashSet<Character> set = new HashSet<Character>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>=97 && s.charAt(i)<=122){
+                set.add(s.charAt(i));
+            }
+        }
+        return (set.size() == 26 ? "pangram" : "not pangram");
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        String result = pangrams(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
