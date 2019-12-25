29af95b64f17ac3cc8d0f02ed852851ea36c6e60
prakash jha <prkjha25@gmail.com>
2019-12-25 11:02:44 +0530

    Create sherlockandarray.java

---
+++ Problem Solving/Easy/sherlockandarray.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class sherlockandarray {
+
+    // Complete the balancedSums function below.
+    static String balancedSums(List<Integer> arr) {
+        int x = 0;
+        int sum = 0;
+        for (int a : arr) {
+            sum += a;
+        }
+
+        for (int y : arr) {
+            if (2 * x == sum - y) {
+                return "YES";
+            }
+            x = x + y;
+        }
+        return "NO";
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int T = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int TItr = 0; TItr < T; TItr++) {
+            int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+            String[] arrItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> arr = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                int arrItem = Integer.parseInt(arrItems[i]);
+                arr.add(arrItem);
+            }
+
+            String result = balancedSums(arr);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
