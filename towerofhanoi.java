74e883c8ad33a47c958f375ff0c4f85b436d09c5
prakash jha <prkjha25@gmail.com>
2020-01-30 01:33:43 +0530

    Update towerofhanoi.java

--- towerofhanoi.java
+++ towerofhanoi.java
@@ -1,22 +1,22 @@
 class towerofhanoi
 { 
     // Java recursive function to solve tower of hanoi puzzle 
-    static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) 
+    static void towerOfHanoi(int n, char A, char B, char C) 
     { 
         if (n == 1) 
         { 
-            System.out.println("Move disk 1 from rod " +  from_rod + " to rod " + to_rod); 
+            System.out.println("Move disk 1 from rod " +  A + " to rod " + B); 
             return; 
         } 
-        towerOfHanoi(n-1, from_rod, aux_rod, to_rod); 
-        System.out.println("Move disk " + n + " from rod " +  from_rod + " to rod " + to_rod); 
-        towerOfHanoi(n-1, aux_rod, to_rod, from_rod); 
+        towerOfHanoi(n-1, A, C, B); 
+        System.out.println("Move disk " + n + " from rod " +  A + " to rod " + B); 
+        towerOfHanoi(n-1, C, B, A); 
     } 
       
     //  Driver method 
     public static void main(String args[]) 
     { 
-        int n = 4; // Number of disks 
+        int n = 3; // Number of disks 
         towerOfHanoi(n, 'A', 'C', 'B');  // A, B and C are names of rods 
     } 
 }
